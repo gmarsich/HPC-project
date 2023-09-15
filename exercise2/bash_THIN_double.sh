eb235142c074ee4c04fcdf4bdc9216606e1e9846
Gaia Marsich <g.marsich@gmail.com>
2023-09-15 21:21:40 +0200

    Update (2)

--- exercise2/bash_THIN_double.sh
+++ exercise2/bash_THIN_double.sh
@@ -12,15 +12,15 @@ end=20000
 step=1000
 
 # Define the number of times to repeat the test (then we will do the average on the runs)
-num_runs=5
+num_runs=7
 
 # Double loop to do more tests (a test is a run), each test is formed by results coming from different inputs
 for ((run = 1; run <= num_runs; run++)); do
     # Loop through inputs and run the C program
     for ((input = start; input <= end; input += step)); do
         output_mkl=$(srun -n1 --cpus-per-task=12 ./gemm_mkl.x "$input" "$input" "$input" | tail -n 1 | grep ",")
-        echo "$output_mkl" >>"${results_dir}/${results_dir}_mkl_${run}.txt"
+        echo "$output_mkl" >>"${results_dir}/${results_dir}_mkl_${run}.csv"
         output_openBLAS=$(srun -n1 --cpus-per-task=12 ./gemm_oblas.x "$input" "$input" "$input" | tail -n 1 | grep ",")
-        echo "$output_openBLAS" >>"${results_dir}/${results_dir}_openBLAS_${run}.txt"
+        echo "$output_openBLAS" >>"${results_dir}/${results_dir}_openBLAS_${run}.csv"
     done
 done
