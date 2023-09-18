0d730db65e9851e89fb8e16c1914f04c454abfe0
Gaia Marsich <g.marsich@gmail.com>
2023-09-18 13:30:22 +0200

    Add Graphs_point2.py

--- exercise2/function_mean_matrix.py
+++ exercise2/function_mean_matrix.py
@@ -7,7 +7,7 @@ def mean_matrix(csv_directory):
 
     # get the dimensions of the 3D matrix: 1° dim: number of tests; 2° dim: number of steps; 3°: number of retained values
     file_path = os.path.join(csv_directory, os.listdir(csv_directory)[0])
-    df = pd.read_csv(file_path, header=None, names=['M', 'N', 'K', 'Elapsed_time_sec', 'GFLOPS'])
+    df = pd.read_csv(file_path, header=None)
     matrix_numpy = df.to_numpy()
 
     # initialise the 3D matrix with zeros
@@ -15,16 +15,16 @@ def mean_matrix(csv_directory):
 
     # save the contents of the csv files in the 3D matrix
     i = -1
-    for filename in os.listdir(csv_directory): # OBS.: observe that the order is random
+    for filename in os.listdir(csv_directory): # OBS.: observe that the order is somehow random
         i+=1
         file_path = os.path.join(csv_directory, filename)
 
         # copy the data of the csv file in a dataframe
-        df = pd.read_csv(file_path, header=None, names=['M', 'N', 'K', 'Elapsed_time_sec', 'GFLOPS'])
+        df = pd.read_csv(file_path, header=None)
         matrix_numpy = df.to_numpy()
         matrix[i] = matrix_numpy
 
     # get the matrix containing the means across the tests
-    mean_matrix = np.mean(matrix, axis=0) # OBS.: the first three columns, containing the values of M, N and K, are "left untouched" by the mean operation since across axis 0 they are all equal
+    mean_matrix = np.mean(matrix, axis=0) # OBS.: the first three or four columns, containing the values of (number cores,) M, N and K, are "left untouched" by the mean operation since across axis 0 they are all equal
     return mean_matrix
     
